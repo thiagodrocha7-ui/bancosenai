c62ce995c3e62ffb25eee60508fb3dcb88182863
thiagodrocha7-ui <thiago.d.rocha7@aluno.senai.br>
2026-09-11 08:26:58 -0300

    feat(documentos):criacao  de controller e model de documentos

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/Cliente.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/Cliente.cs
@@ -4,8 +4,17 @@
     {
         public int CodigoCliente { get; set; }
 
-        public required Cliente { get; set; }
+        public required string nomeCliente { get; set; }
 
-        public
+        public required string Cpf {  get; set; }
+
+        public  int NumeroAgencia { get; set; }
+
+        public int SaldoTotal {  get; set; }
+
+        public required string Sexo {  get; set; }
+        public required string Endereco { get; set; }
+        public required string cidade { get; set;}
+        public required string estado {  get; set; }  
     }
 }
---
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadado.cs
@@ -0,0 +1,6 @@
+﻿namespace BancoSENAIAPI.Controllers
+{
+    public class DocumentoMetadado
+    {
+    }
+}
