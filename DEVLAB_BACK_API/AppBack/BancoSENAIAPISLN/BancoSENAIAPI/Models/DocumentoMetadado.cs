930d596463558c60abfabab6b70ef6a8045fd967
thiagodrocha7-ui <thiago.d.rocha7@aluno.senai.br>
2026-09-11 08:40:04 -0300

    feat(documentos): Simulacao do banco de dados.

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadado.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Models/DocumentoMetadado.cs
@@ -1,6 +1,15 @@
-﻿namespace BancoSENAIAPI.Controllers
+﻿namespace BancoSENAIAPI.Models
 {
     public class DocumentoMetadado
     {
+        public int Id { get; set; }
+
+        public string Name { get; set;}
+
+        public string Extensao { get; set;}
+
+        public string Caminho { get; set;}
+        public int CodigoCliente { get; set;}
+
     }
 }
