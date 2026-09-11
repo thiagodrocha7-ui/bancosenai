18588930b5352ae220d64fed96ee0505ab08e826
thiagodrocha7-ui <thiago.d.rocha7@aluno.senai.br>
2026-09-11 10:27:38 -0300

    feat(documentos): Simulacao do bando de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -6,7 +6,16 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentoController : Controller
     {
+        private readonly string _caminhoRaiz = Path.Combine(
+           Directory.GetCurrentDirectory(),
+           "ClienteArquivos"
+       );
 
-        
+        private static List<Models.DocumentoMetadado> _documentosMetadados = new List<Models.DocumentoMetadado>();
+
+        private static int _nextId = 1;
+
+
+
     }
 }
