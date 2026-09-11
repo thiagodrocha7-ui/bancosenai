ffaf880c18c28cf389a9b90505ef06f6868aa73a
thiagodrocha7-ui <thiago.d.rocha7@aluno.senai.br>
2026-09-11 10:32:09 -0300

    feat(documentos): Criacao diretorio de upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -23,6 +23,12 @@ namespace BancoSENAIAPI.Controllers
             {
                 return BadRequest("Nenhum Arquivo foi enviado.");
             }
+            string pastaCliente = Path.Combine(_caminhoRaiz, codigoCliente.ToString());
+
+            if (!Directory.Exists(pastaCliente))
+            {
+                Directory.CreateDirectory(pastaCliente);
+            }
         }
 
 
